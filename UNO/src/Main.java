73f15ce0d25816223eedc9b769181a2dcb630bcd
apraskurnin_insum <apraskurnin@insum.ca>
2025-01-07 12:11:17 -0500

    1/6/2025 - Created the project. 1/7/2025 - I made a couple basic lines for an introduction and tried to register names for players, but I ran into an issue, so I set that aside and instead began working on paper to try and initialize a shuffled deck of cards.

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //NAME: Katerina Praskurnin
+        //How many players?
+        System.out.println("Welcome to UNO! How many people want to play?");
+        int players = sc.nextInt();
+        //register names for players - IN PROGRESS ( issue - need 4 separate variables )
+        for (int player = 1; player <= players; player++) {
+            //register names for players
+            System.out.println("What is player " + player + "'s name?");
+            String name = sc.nextLine();
+        }
 
+        //establish a changing variable for the card at the top of the deck
+
+        sc.close();
     }
 }
