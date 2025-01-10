d9364ae22550d7dee8519fe7cbe9b441efaa47bd
apraskurnin_insum <apraskurnin@insum.ca>
2025-01-10 11:00:50 -0500

    1/6/2025 - Created the project. 1/7/2025 - I made a couple basic lines for an introduction and tried to register names for players, but I ran into an issue, so I set that aside and instead began working on paper to try and initialize a shuffled deck of cards. 1/10/2025 - I began implementing the shuffled deck strategy I thought up earlier on paper.

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -1,20 +1,64 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //NAME: Katerina Praskurnin
         //How many players?
-        System.out.println("Welcome to UNO! How many people want to play?");
-        int players = sc.nextInt();
-        //register names for players - IN PROGRESS ( issue - need 4 separate variables )
-        for (int player = 1; player <= players; player++) {
-            //register names for players
-            System.out.println("What is player " + player + "'s name?");
-            String name = sc.nextLine();
-        }
+//        System.out.println("Welcome to UNO! How many people want to play?");
+//     int players = sc.nextInt();
+//     register names for players - IN PROGRESS ( issue - need 4 separate variables )
+//        for (int player = 1; player <= players; player++) {
+//            //register names for players
+//            System.out.println("What is player " + player + "'s name?");
+//            String name = sc.nextLine();
+//        }
 
         //establish a changing variable for the card at the top of the deck
-        //
+        String topCard = "";
+        //establish states
+        boolean special = false;
+        boolean wild = false;
+        boolean red = false;
+        boolean yellow = false;
+        boolean blue = false;
+        boolean green = false;
+        boolean draw = false;
+        boolean skip = false;
+        boolean wildDraw4 = false;
+        //might not need these
+        //initialize array 1-100
+        String card = "";
+        ArrayList<String> deck = new ArrayList<String>();
+        for ( int i = 0; i<100; i++){
+            int shuffler = (int)(Math.random() * (100 - 1 + 1) + 1);
+            if (shuffler<24) {
+                special = true;
+                card += "S";
+                shuffler = (int) (Math.random() * (8 - 1 + 1) + 1);
+                if (shuffler < 4) {
+                    wild = true;
+                    card += "W";
+                } else {
+                    wildDraw4 = true;
+                    card += "D";
+                }
+            }
+            else {
+
+
+            }
+            }
+            deck.add("");
+
+        }
+
+        //pick from array.size
+        //clear the picked one
+        //if picked one is greater than 24, boolean special = false
+        // ig less than 24, special = true
+        // array out of 8, pick random, if
+
         sc.close();
     }
 }
