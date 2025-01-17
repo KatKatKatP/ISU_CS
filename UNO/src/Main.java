3986767af6341c7647177f29deed5c2b012a5002
apraskurnin_insum <apraskurnin@insum.ca>
2025-01-17 10:08:04 -0500

    1/6/2025 - Created the project. 1/7/2025 - I made a couple basic lines for an introduction and tried to register names for players, but I ran into an issue, so I set that aside and instead began working on paper to try and initialize a shuffled deck of cards. 1/10/2025 - I began implementing the shuffled deck strategy I thought up earlier on paper. 1/13/2025 - worked on the deck more as I want to catch up a lot more, i've been a little behind 1/14/2025 - worked on the deck, almost done it. It's not working at all which is unfortunate. I may need to change my strategy. 1/15/2025 - im running in to a lot of issues 1/16/2025 - asked for help, tomorrow i'm going to first initialize an unshuffled deck and then shuffle it.

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -13,251 +13,137 @@ public class Main {
 //            System.out.println("What is player " + player + "'s name?");
 //            String name = sc.nextLine();
 //        }
-//
-//        //establish a changing variable for the card at the top of the deck
-//        String topCard = "";
-//        //establish states
-//        boolean special = false;
-//        boolean wild = false;
-//        boolean colorSpecial = false;
-//        boolean red = false;
-//        boolean yellow = false;
-//        boolean blue = false;
-//        boolean green = false;
-//        boolean draw = false;
-//        boolean skip = false;
-//        boolean wildDraw4 = false;
-//        //might not need these
-//        //initialize array 1-100
-//
-//        ArrayList<Integer> countSpecialcolor = new ArrayList<Integer>();
-//        for(int j = 1; j <= 16; j++) {
-//            countSpecialcolor.add(j);
-//        }
-//        ArrayList<String> deck = new ArrayList<String>();
-//        for (int i = 0; i < 100; i++) {
-//            String card = "";
-//            int shuffler = (int) (Math.random() * (100 - 1 + 1) + 1);
-//            if (shuffler < 24) {
-//                special = true;
-//                card += "S";
-//                shuffler = (int) (Math.random() * (24 - 1 + 1) + 1);
-//                if (shuffler > 8) {
-//                    colorSpecial = true;
-//                    card += "C";
-//                    shuffler = (int) (Math.random() * (16 - 1 + 1) + 1);
-//                    switch(shuffler){
-//                        case 1:
-//                            if (countSpecialcolor.contains(1)) {
-//                                red = true;
-//                                card += "RD";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(1));
-//                            }
-//                            break;
-//                        case 2:
-//                             if (countSpecialcolor.contains(2)) {
-//                                red = true;
-//                                card += "RD";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(2));
-//                            }
-//                            break;
-//                        case 3:
-//                            if (countSpecialcolor.contains(3)) {
-//                                red = true;
-//                                card += "RS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(3));
-//                            }
-//                            break;
-//                        case 4:
-//                           if (countSpecialcolor.contains(4)) {
-//                                red = true;
-//                                card += "RS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(4));
-//                            } break;
-//                        case 5:
-//                            if (countSpecialcolor.contains(5)) {
-//                                card += "YD";
-//                                yellow = true;
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(5));
-//                            }
-//                            break;
-//                        case 6:
-//                             if (countSpecialcolor.contains(6)) {
-//                                yellow = true;
-//                                card += "YD";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(6));
-//                            }
-//                            break;
-//                        case 7:
-//                            if (countSpecialcolor.contains(7)) {
-//                                yellow = true;
-//                                card += "YS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(7));
-//                            }
-//                            break;
-//                        case 8:
-//                          if (countSpecialcolor.contains(8)) {
-//                                yellow = true;
-//                                card += "YS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(8));
-//                            }
-//                            break;
-//                        case 9:
-//                            if (countSpecialcolor.contains(9)) {
-//                                blue = true;
-//                                card += "BL";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(9));
-//                            }
-//                            break;
-//
-//                        case 10:
-//                            if (countSpecialcolor.contains(10)) {
-//                                blue = true;
-//                                card += "BL";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(10));
-//                            }
-//
-//                            break;
-//                        case 11:
-//                            if (countSpecialcolor.contains(11)) {
-//                                blue = true;
-//                                card += "BS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(11));
-//                            }
-//                            break;
-//                        case 12:
-//                             if (countSpecialcolor.contains(12)) {
-//                                blue = true;
-//                                card += "BS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(12));
-//                            }
-//                            break;
-//                        case 13:
-//                            if (countSpecialcolor.contains(13)) {
-//                                green = true;
-//                                card += "GD";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(13));
-//                            }
-//                            break;
-//                        case 14:
-//                            if (countSpecialcolor.contains(14)) {
-//                                green = true;
-//                                card += "GD";
-//                                draw = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(14));
-//                            }
-//                            break;
-//                        case 15:
-//                            if (countSpecialcolor.contains(15)) {
-//                                green = true;
-//                                card += "GS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(15));
-//                            }
-//                            break;
-//                        case 16:
-//                            if (countSpecialcolor.contains(16)) {
-//                                green = true;
-//                                card += "GS";
-//                                skip = true;
-//                                countSpecialcolor.remove(countSpecialcolor.indexOf(16));
-//                            }
-//                            break;
-//                        default:
-//                            i --;
-//                        }
-//                }
-//                else{
-//                    if (shuffler < 4) {
-//                        wild = true;
-//                        card += "W";
-//                    } else {
-//                        wildDraw4 = true;
-//                        card += "D4";
-//                    }
-//                }
-//            }
-//             if (shuffler > 24) {
-//                shuffler = (int) (Math.random() * (76 - 1 + 1) + 1);
-//                if (shuffler <= 19) {
-//                    red = true;
-//                    card += "R";
-//                }
-//                if (shuffler > 19 && shuffler <= 38) {
-//                    green = true;
-//                    card += "G";
-//                }
-//                if (shuffler > 38 && shuffler <= 57) {
-//                    yellow = true;
-//                    card += "Y";
-//                }
-//                if (shuffler > 57 && shuffler <= 76) {
-//                    blue = true;
-//                    card += "B";
-//                }
-//
-//                shuffler = (int) (Math.random() * (19 - 1 + 1) + 1);
-//                switch (shuffler) {
-//                    case 1:
-//                        card += 0;
-//                        break;
-//                    case 2:
-//                    case 3:
-//                        card += 1;
-//                        break;
-//                    case 4:
-//                    case 5:
-//                        card += 2;
-//                        break;
-//                    case 6:
-//                    case 7:
-//                        card += 3;
-//                        break;
-//                    case 8:
-//                    case 9:
-//                        card += 4;
-//                        break;
-//                    case 10:
-//                    case 11:
-//                        card += 5;
-//                        break;
-//                    case 12:
-//                    case 13:
-//                        card += 6;
-//                        break;
-//                    case 14:
-//                    case 15:
-//                        card += 7;
-//                        break;
-//                    case 16:
-//                    case 17:
-//                        card += 8;
-//                        break;
-//                    case 18:
-//                    case 19:
-//                        card += 9;
-//                        break;
-//                }
-//            }
-//            deck.add(card);
-//
-//        }
-//        System.out.println(deck);
-//        sc.close();
 
+        ArrayList<String> deck = new ArrayList<String>();
+        String card = "";
+        for (int i = 1; i <= 100; i++){
+            if (i<=24){
+                card += "S";
+                if (i<4){
+                    card += "D4";
+                }
+                else if (i <=8 && i >4){
+                    card +="W";
+                }
+                switch (i){
+                    case 1:
+                    case 2:
+                        card += "RD";
+                        break;
+                    case 3:
+                    case 4:
+                        card += "RS";
+                        break;
+                    case 5:
+                    case 6:
+                        card += "YD";
+                        break;
+                    case 7:
+                    case 8:
+                        card += "YS";
+                        break;
+                    case 9:
+                    case 10:
+                        card += "BD";
+                        break;
+                    case 11:
+                    case 12:
+                        card += "BS";
+                        break;
+                    case 13:
+                    case 14:
+                        card += "GD";
+                        break;
+                    case 15:
+                    case 16:
+                        card += "GS";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else{
+                if (i<= 44){
+                    card += "R";
+                    switch(i){
+                        case 25:
+                            card += "0"; break;
+                        case 26:
+                        case 27:
+                            card += "1"; break;
+                        case 28:
+                        case 29:
+                            card += "2"; break;
+                        case 30:
+                        case 31:
+                            card += "3"; break;
+                        case 32:
+                        case 33:
+                            card += "4"; break;
+                        case 34:
+                        case 35:
+                            card += "5"; break;
+                        case 37:
+                        case 38:
+                            card += "6"; break;
+                        case 39:
+                        case 40:
+                            card += "7"; break;
+                        case 41:
+                        case 42:
+                            card += "8"; break;
+                        case 43:
+                        case 44:
+                            card += "9"; break;
+                        default:
+                            break;
+                    }
+                }
+                else if(i<= 63){
+                    card += "G";
+                    switch(i){
+                        case 45:
+                            card += "0"; break;
+                        case 46:
+                        case 47:
+                            card += "1"; break;
+                        case 48:
+                        case 49:
+                            card += "2"; break;
+                        case 50:
+                        case 51:
+                            card += "3"; break;
+                        case 52:
+                        case 53:
+                            card += "4"; break;
+                        case 54:
+                        case 55:
+                            card += "5"; break;
+                        case 56:
+                        case 57:
+                            card += "6"; break;
+                        case 58:
+                        case 59:
+                            card += "7"; break;
+                        case 60:
+                        case 61:
+                            card += "8"; break;
+                        case 62:
+                        case 63:
+                            card += "9"; break;
+                        default:
+                            break;
+                    }
+                }
+                else if (i<= 81){
+                    card += "Y";
+                }
+                else{
+                    card += "B";
+                }
+            }
 
+        }
+         System.out.println(deck);
     }
 }
